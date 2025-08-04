a4c09d4e3fabb3ffa4e89e1e6c711068697bf994
Sarthak Bhatnagar <144317092+sarthakbhatnagar2002@users.noreply.github.com>
2025-08-04 17:04:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-function connectToDB(){
-    mongoose.connect(process.env.MONGO_URI).then(()=>
-        console.log('Connected to DB')
-    )
+function connectToDB() {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => {
+    console.error('DB connection error:', err);
+    process.exit(1);
+  });
 }
 
 module.exports = connectToDB;
